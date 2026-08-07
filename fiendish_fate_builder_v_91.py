c95fbbb095069b1bce3c8648c1a226cf3d1b39bd
Bright-Goblin <johnson_76@spectrum.net>
2026-08-07 16:16:34 -0500

    Update fiendish_fate_builder_v_91.py

--- fiendish_fate_builder_v_91.py
+++ fiendish_fate_builder_v_91.py
@@ -44,19 +44,18 @@ initiative = st.number_input("Initiative", 0, 100, 6)
 # --- Attributes ---
 st.header("Attributes")
 col_a1, col_a2, col_a3, col_a4 = st.columns(4)
-#col_a1, col_a2 = st.columns(4)
 with col_a1:
     str_score = st.number_input("STR", 1, 40, 10)
-    pow_score = st.number_input("POW", 1, 40, 10)
 #    int_score = st.number_input("INT", 1, 40, 10)
 with col_a2:
     dex_score = st.number_input("DEX", 1, 40, 10)
-    tou_score = st.number_input("TOU", 1, 40, 10)
 #    con_score = st.number_input("CON", 1, 40, 10)
 # with col_a3:
+    pow_score = st.number_input("POW", 1, 40, 10)
 #    pow_score = st.number_input("POW", 1, 40, 10)
 #    cha_score = st.number_input("CHA", 1, 40, 10)
 # with col_a4:
+    tou_score = st.number_input("TOU", 1, 40, 10)
 #    tou_score = st.number_input("TOU", 1, 40, 10)
 #tou_score = st.number_input("TOU", 1, 40, 10)
 #tou_cost = tou_score - 8
